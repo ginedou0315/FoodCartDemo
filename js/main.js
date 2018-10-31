8cd5aaecd4b6e6087913f8cfbdc5a1fa10fc3f8b
Rosie L <40253226+rosielash@users.noreply.github.com>
2018-10-30 21:41:53 -0700

    Gallery Images
    Added images and hover effects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 		
+
+		
+		
 		function myMap() {
 		var mapProp= {
 			center:new google.maps.LatLng(51.508742,-0.120850),
@@ -6,6 +9,8 @@
 		};
 		var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 		}
+		
+
 		
 
 		
