4b2fa2913bea810ecf252bf29ce97d7a401c5a51
Rosie L <40253226+rosielash@users.noreply.github.com>
2018-11-05 15:05:34 -0800

    FC-14
    Changed carousel buttons to border shadow only

--- js/main.js
+++ js/main.js
@@ -19,7 +19,18 @@
 		}
 	
 	// END slide show//
-		
+	
+	// Nav Function//
+	
+	function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
+	// END Nav Function//		
 		
 		function myMap() {
 		var mapProp= {
